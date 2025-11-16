13979ef2c0a7e82a3062dfa7e1ef14bf7fd3392a
nevasthno <rashevco@gmail.com>
2025-11-16 20:05:06 +0200

    backup nginx.conf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "terraform.tfstate-the-5h3y5rtfs"
+    bucket       = "terraform-state-fo-gepuonlcjhv"
     key          = "terraform.tfstate"
     region       = "eu-central-1"
     use_lockfile = true
